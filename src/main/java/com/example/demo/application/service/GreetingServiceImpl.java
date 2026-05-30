048f38c0d7be1af4013cfa69da660035f2bc7dc5
Vitoco <vitoco@192.168.1.119>
2026-05-30 16:01:15 -0400

    feat: cambiar mensaje de greeting a V7

--- src/main/java/com/example/demo/application/service/GreetingServiceImpl.java
+++ src/main/java/com/example/demo/application/service/GreetingServiceImpl.java
@@ -24,7 +24,7 @@ public class GreetingServiceImpl implements CreateGreetingUseCase, LifecycleUseC
         if (name == null || name.isBlank()) {
             name = "World";
         }
-        String message = "Hola DevOps V6, " + name + "!";
+        String message = "Hola DevOps V7, " + name + "!";
         Greeting greeting = new Greeting(null, name, message, Instant.now());
         return greetingRepositoryPort.save(greeting);
     }
