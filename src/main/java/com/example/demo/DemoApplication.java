40acf043279237696329ff75ad012387ad8a2261
Vitoco <vitoco@192.168.1.27>
2026-07-01 21:53:40 -0400

    ci: trigger pipeline to validate devops architecture and fail-fast triggers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// Trigger pipeline EP3 - Validando Fail-Fast y despliegue automatizado EC2
+
 public class DemoApplication {
 
     public static void main(String[] args) {
